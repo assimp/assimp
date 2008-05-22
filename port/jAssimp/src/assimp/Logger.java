245bc166897a8b035220cf1853c223ac146deccd
aramis_acg <aramis_acg@67173fc5-114c-0410-ac8e-9d2fd5bffc1f>
2008-05-22 10:27:36 +0000

    Changed "Free" to "Open", Java build configs, port directory added, intellij idea5.1 project file added
    git-svn-id: https://assimp.svn.sourceforge.net/svnroot/assimp/trunk@26 67173fc5-114c-0410-ac8e-9d2fd5bffc1f

---
+++ port/jAssimp/src/assimp/Logger.java
@@ -0,0 +1,11 @@
+package assimp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alex
+ * Date: 21.05.2008
+ * Time: 19:31:28
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Logger {
+}

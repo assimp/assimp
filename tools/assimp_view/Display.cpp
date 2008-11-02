0f56c48aca6d468a014fdb18a472027f3cf57da6
ulfjorensen <ulfjorensen@67173fc5-114c-0410-ac8e-9d2fd5bffc1f>
2008-11-02 19:34:24 +0000

    - finally found the last bug in the viewer animation code. Dwarf is now dwarfing, Tiny is now running correctly.
    git-svn-id: https://assimp.svn.sourceforge.net/svnroot/assimp/trunk@220 67173fc5-114c-0410-ac8e-9d2fd5bffc1f

--- tools/assimp_view/Display.cpp
+++ tools/assimp_view/Display.cpp
@@ -1988,7 +1988,7 @@ int CDisplay::RenderNode (aiNode* piNode,const aiMatrix4x4& piMatrix,
 					*tempmat++ = mat.d1; *tempmat++ = mat.d2; *tempmat++ = mat.d3; *tempmat++ = mat.d4; 
 					//tempmat += 4;
 				}
-				helper->piEffect->SetMatrixArray( "gBoneMatrix", (D3DXMATRIX*)matrices, 60);
+				helper->piEffect->SetMatrixTransposeArray( "gBoneMatrix", (D3DXMATRIX*)matrices, 60);
 			}
 
 			// now setup the material

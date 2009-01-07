b87e572220c334599ccfb3113a9120f47ea7d5cb
aramis_acg <aramis_acg@67173fc5-114c-0410-ac8e-9d2fd5bffc1f>
2009-01-07 15:04:15 +0000

    Added batch script to copy DLL release exe's and dll to a common 'final' directory.
    git-svn-id: https://assimp.svn.sourceforge.net/svnroot/assimp/trunk@267 67173fc5-114c-0410-ac8e-9d2fd5bffc1f

---
+++ bin/mkfinal.bat
@@ -0,0 +1,6 @@
+mkdir final
+
+copy assimp_release-dll_win32\assimp32.dll final\assimp32.dll
+
+copy assimpview_release-dll_win32\assimp_view.exe final\assimp_view.exe
+copy assimpdumb_release-dll_win32\assimp_dumb.exe final\assimp_dumb.exe

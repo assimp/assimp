a4d7871096cd97fee8a7af0477e7ee0e7ac98a7f
aramis_acg <aramis_acg@67173fc5-114c-0410-ac8e-9d2fd5bffc1f>
2009-06-20 14:54:50 +0000

    [2805689] GCC 4.4 compatibility.
    git-svn-id: https://assimp.svn.sourceforge.net/svnroot/assimp/trunk@443 67173fc5-114c-0410-ac8e-9d2fd5bffc1f

--- code/AssimpPCH.h
+++ code/AssimpPCH.h
@@ -116,6 +116,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <algorithm>
 #include <numeric>
 #include <new>
+#include <cstdio>
 
 // Public ASSIMP headers
 #include "../include/DefaultLogger.h"

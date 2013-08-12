fe248c89ecd2383c977558579af3fc6ea524e1c8
Alexander Gessler <alexander.gessler@gmx.net>
2013-08-12 23:42:46 +0200

    Exporter.hpp: predeclare IOSystem

--- include/assimp/Exporter.hpp
+++ include/assimp/Exporter.hpp
@@ -51,6 +51,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace Assimp	{
 	class ExporterPimpl;
+	class IOSystem;
 
 
 // ----------------------------------------------------------------------------------

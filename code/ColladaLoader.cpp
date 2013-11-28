2eb8189e505b286c99dcd6487af0f1365b2d2b90
henrikbuchholz <henrik.buchholz@autodesk.com>
2013-11-28 16:43:56 +0100

    Removed duplicate unitSize scaling (DE7221)
    Since last AssImp update, AssImp did the unit scaling itself already (see 160). Therefore, our old fix became a bug after update.
    => Reverted our local modification.
    
    (Also checked in ColladaParser that the -1.0 check is not needed, because the Parser sets 1 if the attribute is not found)

--- code/ColladaLoader.cpp
+++ code/ColladaLoader.cpp
@@ -178,14 +178,6 @@ void ColladaLoader::InternReadFile( const std::string& pFile, aiScene* pScene, I
 				 0,  0,  0,  1);
         }
 
-    // Convert to meter
-    if( parser.mUnitSize != 1.f )
-        pScene->mRootNode->mTransformation *= aiMatrix4x4( 
-            parser.mUnitSize,  0,  0,  0, 
-            0,  parser.mUnitSize,  0,  0,
-            0,  0,  parser.mUnitSize,  0,
-            0,  0,  0,  1               );
-
 	// store all meshes
 	StoreSceneMeshes( pScene);
 

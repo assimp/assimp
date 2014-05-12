0c4752fbe174da0892ee6353ffc5f539aaf2ca74
henrikbuchholz <henrik.buchholz@autodesk.com>
2014-05-12 09:19:10 +0200

    Made BuildAssImp batch file work with VisualStudio Express
    (actual work done by  Himanshu)

--- BuildAssImpForInfraWorks.bat
+++ BuildAssImpForInfraWorks.bat
@@ -5,19 +5,14 @@ Rem  VisualStudio-Version: VS2012
 Rem  Solution-Configs:     debug-noboost-dll, release-noboost-dll
 Rem  Project:              assimp (assimpview etc. are not required)
 
-Rem Find VisualStudio 2012 devenv file
-Set DevEnvDir=%VS110COMNTOOLS%\..\IDE
+set solutionFile=workspaces\vc11\assimp.sln
 
-if not exist "%DevEnvDir%\devenv.com" GOTO :ERROR_DONE
+call MSBuild.exe %solutionFile% /property:Configuration=debug-noboost-dll /property:Platform=x64 /target:assimp
+if errorlevel 1 (
+	exit /B errorlevel
+)
 
-"%DevEnvDir%\devenv.com" workspaces\vc11\assimp.sln /build "debug-noboost-dll|x64" /project assimp
-"%DevEnvDir%\devenv.com" workspaces\vc11\assimp.sln /build "release-noboost-dll|x64" /project assimp
-
-GOTO :EOF
-
-:ERROR_DONE
-
-echo Failed to build AssImp library, because devenv.com could not be located.
-echo Make sure that the VS110COMNTOOLS env variable is properly set e.g. to 
-echo C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\
-echo This should usually be set automatically when installing VisualStudio 2012
+call MSBuild.exe %solutionFile% /property:Configuration=release-noboost-dll /property:Platform=x64 /target:assimp
+if errorlevel 1 (
+	exit /B errorlevel
+)

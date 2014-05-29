fe87b092467b32104a146b43a2d93f6e219f0562
Jonne Nauha <jonne.nauha@adminotech.com>
2014-05-30 01:57:21 +0300

    Move pstdint.h for <= VC9 under include. Apps that include the Assimp headers need to have this available, as its pulled in by metadata.h. Fixes #290.

--- include/assimp/metadata.h
+++ include/assimp/metadata.h
@@ -48,7 +48,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assert.h>
 
 #if defined(_MSC_VER) && (_MSC_VER <= 1500)
-#include "pstdint.h"
+#include "Compiler/pstdint.h"
 #else
 #include <stdint.h>
 #endif

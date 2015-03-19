abe82a5d73e404d7ead988c87dc9460dc68a4620
Kim Kulling <kim.kulling@googlemail.com>
2015-03-19 12:34:11 +0100

    add missing include
    Signed-off-by: Kim Kulling <kim.kulling@googlemail.com>

--- contrib/openddlparser/code/OpenDDLParser.cpp
+++ contrib/openddlparser/code/OpenDDLParser.cpp
@@ -26,6 +26,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <iostream>
 #include <sstream>
 #include <algorithm>
+#include <math.h>
 
 #ifdef _WIN32
 #  include <windows.h>

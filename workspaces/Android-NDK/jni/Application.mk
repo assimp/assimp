23797dc8ccc7ec3bf03fd6b25e6d3223ffc95623
Ser Lev Arris <arris@zsolttech.com>
2015-03-20 21:15:57 +0100

    for sure jassimp needs the assimp lib too

---
+++ workspaces/Android-NDK/jni/Application.mk
@@ -0,0 +1,11 @@
+# NDK_TOOLCHAIN_VERSION=clang # fails on IFCBoolean.cpp and GEOColorTable.h ???
+#NDK_TOOLCHAIN_VERSION=4.8
+#NDK_TOOLCHAIN_VERSION=4.7
+#NDK_TOOLCHAIN_VERSION=4.6
+
+APP_ABI := all
+APP_ABI := x86
+
+APP_STL := stlport_static
+
+#NDK_APP_OUT := /home/Video-HD/C-Backups/arris69_assimp.git/JassimpLibrary

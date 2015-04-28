91c751a03aecca0d948e71e4cc85029095f79933
Léo Terziman <leo.terziman@clarte.asso.fr>
2015-04-28 12:29:39 +0200

    Added missing header

--- code/ColladaParser.cpp
+++ code/ColladaParser.cpp
@@ -46,6 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef ASSIMP_BUILD_NO_COLLADA_IMPORTER
 
+#include <sstream>
 #include "ColladaParser.h"
 #include "fast_atof.h"
 #include "ParsingUtils.h"

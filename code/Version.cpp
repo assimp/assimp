655cb65ab2b91714f06d058a70658d42df0153dd
Kim Kulling <kim.kulling@googlemail.com>
2015-11-03 19:43:02 +0100

    Version: increase version from 3.1.1 to 3.2.

--- code/Version.cpp
+++ code/Version.cpp
@@ -6,7 +6,7 @@
 #include "ScenePrivate.h"
 
 static const unsigned int MajorVersion = 3;
-static const unsigned int MinorVersion = 1;
+static const unsigned int MinorVersion = 2;
 
 // --------------------------------------------------------------------------------
 // Legal information string - dont't remove this.

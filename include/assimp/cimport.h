c02a1dd11a43d47f3d6b3521178ff230efd02362
Kim Kulling <kim.kulling@googlemail.com>
2016-01-20 21:25:47 +0100

    closes assimp/assimp/issues/710: use correct include for importerdesc in cimport.h

--- include/assimp/cimport.h
+++ include/assimp/cimport.h
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef AI_ASSIMP_H_INC
 #define AI_ASSIMP_H_INC
 #include "types.h"
-#include <assimp/importerdesc.h>
+#include "importerdesc.h"
 
 #ifdef __cplusplus
 extern "C" {

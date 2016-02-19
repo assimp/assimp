1558db0722011c134cb9946f9d1a15aac8e1c731
Kim Kulling <kim.kulling@googlemail.com>
2016-02-19 11:44:02 +0100

    appveyor: remove buggy quotes.

--- scripts/appveyor/compiler_setup.bat
+++ scripts/appveyor/compiler_setup.bat
@@ -3,7 +3,7 @@ rem @echo off
 :: Now we declare a scope
 Setlocal EnableDelayedExpansion EnableExtensions
 
-if not defined Configuration set Configuration=2015
+if not defined Configuration set Configuration=14 2015
 
 if "%Configuration%"=="MinGW" ( goto :mingw )
 
@@ -11,24 +11,20 @@ set arch=x86
 
 if "%platform%" EQU "x64" ( set arch=x86_amd64 )
 
-if "%Configuration%"=="2015" (
+if "%Configuration%"=="14 2015" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
 
-if "%Configuration%"=="2013" (
+if "%Configuration%"=="12 2013" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 )
 
-if "%Configuration%"=="2012" (
+if "%Configuration%"=="11 2012" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 )
 
-if "%Configuration%"=="2010" (
+if "%Configuration%"=="10 2010" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-)
-
-if "%Configuration%"=="2008" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 )
 
 :: Visual Studio detected

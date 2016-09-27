b73f72ccbf491eb14daed482c7b0291e37bd27ed
DavidARivkin <davidarivkin@gmail.com>
2016-09-27 17:42:06 +0300

    Moves header and fixed instantiation
    The SceneCombiner is a very useful utility class for applications.  I
    have moved it to the include directory and fixed the instantiation
    problem.

--- include/assimp/SceneCombiner.h
+++ include/assimp/SceneCombiner.h
@@ -46,7 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <assimp/ai_assert.h>
 #include <assimp/types.h>
-#include "Defines.h"
+//#include "Defines.h"
 #include <stddef.h>
 #include <set>
 #include <list>
@@ -200,10 +200,8 @@ struct SceneHelper
  */
 class SceneCombiner
 {
-    // class cannot be instanced
-    SceneCombiner() {}
-
 public:
+    SceneCombiner() {}
 
     // -------------------------------------------------------------------
     /** Merges two or more scenes.

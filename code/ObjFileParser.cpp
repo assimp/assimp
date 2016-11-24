bd6f94b9267ba2dcdadb0484d353c3b891667f44
Albin Bernhardsson <catscratchesp@gmail.com>
2016-11-24 14:15:13 +0100

    Remove unused variable

--- code/ObjFileParser.cpp
+++ code/ObjFileParser.cpp
@@ -101,8 +101,6 @@ ObjFile::Model *ObjFileParser::GetModel() const {
 // -------------------------------------------------------------------
 //  File parsing method.
 void ObjFileParser::parseFile( IOStreamBuffer<char> &streamBuffer ) {
-    // only update every 100KB or it'll be too slow
-    const unsigned int updateProgressEveryBytes = 100 * 1024;
     unsigned int progressCounter = 0;
     const unsigned int bytesToProcess = streamBuffer.size();
     const unsigned int progressTotal = 3 * bytesToProcess;

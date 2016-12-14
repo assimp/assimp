bd0449ea79b132a0791c3afe7e2661f7f678f3cf
Kim Kulling <kim.kulling@googlemail.com>
2016-12-14 21:42:01 +0100

    closes assimp/assimp/issues/1101: copy aiTexture type declaration instead of using decltype for declaration to fix iOS build.

--- code/AMFImporter.hpp
+++ code/AMFImporter.hpp
@@ -135,18 +135,18 @@ private:
 	};
 
 	/// \struct SPP_Texture
-	/// Data type for postprocessing step. More suitable container for texture.
+	/// Data type for post-processing step. More suitable container for texture.
 	struct SPP_Texture
 	{
 		std::string ID;
-		size_t Width, Height, Depth;
-		bool Tiled;
-		decltype(aiTexture::achFormatHint) FormatHint;
-		uint8_t* Data;
+		size_t      Width, Height, Depth;
+		bool        Tiled;
+        char        FormatHint[ 9 ];// 8 for string + 1 for terminator.
+		uint8_t    *Data;
 	};
 
 	///	\struct SComplexFace
-	/// Data type for postprocessing step. Contain face data.
+	/// Data type for post-processing step. Contain face data.
 	struct SComplexFace
 	{
 		aiFace Face;///< Face vertices.

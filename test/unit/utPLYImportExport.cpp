616a18b0c77a9cc3614806bd20252f6ac00ef776
Kim Kulling <kim.kulling@googlemail.com>
2017-06-09 21:57:41 +0200

    Unittests: fix uppercase typo in export path for ply export test.

--- test/unit/utPLYImportExport.cpp
+++ test/unit/utPLYImportExport.cpp
@@ -60,7 +60,7 @@ public:
         Exporter exporter;
         const aiScene *scene = importer.ReadFile(ASSIMP_TEST_MODELS_DIR "/PLY/cube.ply", 0);
         EXPECT_NE(nullptr, scene);
-        EXPECT_EQ(aiReturn_SUCCESS, exporter.Export(scene, "ply", ASSIMP_TEST_MODELS_DIR "/ply/cube_test.ply"));
+        EXPECT_EQ(aiReturn_SUCCESS, exporter.Export(scene, "ply", ASSIMP_TEST_MODELS_DIR "/PLY/cube_test.ply"));
 
         return true;
     }

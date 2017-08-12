b1313b04b8274106a3affd42db7c72d0bce8e5ff
Jeremy Abel <jeremyabel@gmail.com>
2017-08-12 00:16:00 -0400

    should be map, not set

--- code/C4DImporter.h
+++ code/C4DImporter.h
@@ -47,7 +47,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "BaseImporter.h"
 #include "LogAux.h"
 
-#include <set>
+#include <map>
 struct aiNode;
 struct aiMesh;
 struct aiMaterial;

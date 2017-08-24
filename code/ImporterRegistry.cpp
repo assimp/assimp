63d3655f1b8b73e3c15799e061baaa8fa95e086f
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-08-24 17:15:48 -0400

    Duplicate gltfImporter as gltf2Importer; Include glTF2 importer in CMake List

--- code/ImporterRegistry.cpp
+++ code/ImporterRegistry.cpp
@@ -181,7 +181,7 @@ corresponding preprocessor flag to selectively disable formats.
 #   include "AssbinLoader.h"
 #endif
 #ifndef ASSIMP_BUILD_NO_GLTF_IMPORTER
-#   include "glTFImporter.h"
+#   include "glTF2Importer.h"
 #endif
 #ifndef ASSIMP_BUILD_NO_C4D_IMPORTER
 #   include "C4DImporter.h"
@@ -335,7 +335,7 @@ void GetImporterInstanceList(std::vector< BaseImporter* >& out)
     out.push_back( new AssbinImporter() );
 #endif
 #if ( !defined ASSIMP_BUILD_NO_GLTF_IMPORTER )
-    out.push_back( new glTFImporter() );
+    out.push_back( new glTF2Importer() );
 #endif
 #if ( !defined ASSIMP_BUILD_NO_C4D_IMPORTER )
     out.push_back( new C4DImporter() );

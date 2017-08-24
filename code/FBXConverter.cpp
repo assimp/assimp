80489963a15de3eea59810dbdbdeabdd6c103501
Tammo Hinrichs <tammo.hinrichs@ventuz.com>
2017-08-24 17:18:54 +0200

    FBX importer: don't rely ont INT64_MIN / ..MAX macros

--- code/FBXConverter.cpp
+++ code/FBXConverter.cpp
@@ -2371,9 +2371,9 @@ void Converter::ConvertAnimationStack( const AnimationStack& st )
     bool has_local_startstop = start_time != 0 || stop_time != 0;
     if ( !has_local_startstop ) {
         // no time range given, so accept every keyframe and use the actual min/max time
-        // the +/- 20000 is a safety because GenerateNodeAnimations uses an epsilon of 10000
-        start_time = INT64_MIN + 20000;
-        stop_time = INT64_MAX - 20000;
+        // the numbers are INT64_MIN/MAX, the 20000 is for safety because GenerateNodeAnimations uses an epsilon of 10000
+        start_time = -9223372036854775807i64 + 20000;
+        stop_time = 9223372036854775807i64 - 20000;
     }
 
     try {

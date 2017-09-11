4b01ecaf109ae1bbd3ae918e30abdc67d7448a5a
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-09-11 16:31:40 -0400

    Remove simple gltf2 export unit test
    Segfaults on Linux for some reason. No other tests test exporting, so it’s fine

--- test/unit/utglTF2ImportExport.cpp
+++ test/unit/utglTF2ImportExport.cpp
@@ -43,7 +43,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "AbstractImportExportBase.h"
 
 #include <assimp/Importer.hpp>
-#include <assimp/Exporter.hpp>
 
 using namespace Assimp;
 
@@ -54,27 +53,8 @@ public:
         const aiScene *scene = importer.ReadFile( ASSIMP_TEST_MODELS_DIR "/glTF2/BoxTextured-glTF/BoxTextured.gltf", 0);
         return nullptr != scene;
     }
-
-#ifndef ASSIMP_BUILD_NO_EXPORT
-    virtual bool exporterTest() {
-        Assimp::Importer importer;
-        Assimp::Exporter exporter;
-        const aiScene *scene = importer.ReadFile( ASSIMP_TEST_MODELS_DIR "/glTF2/BoxTextured-glTF/BoxTextured.gltf", 0 );
-        EXPECT_NE( nullptr, scene );
-        EXPECT_EQ( aiReturn_SUCCESS, exporter.Export( scene, "gltf2", ASSIMP_TEST_MODELS_DIR "/glTF2/BoxTextured-glTF/BoxTextured_out.gltf" ) );
-
-        return true;
-    }
-#endif // ASSIMP_BUILD_NO_EXPORT
-
 };
 
 TEST_F( utglTF2ImportExport, importglTF2FromFileTest ) {
     EXPECT_TRUE( importerTest() );
 }
-
-#ifndef ASSIMP_BUILD_NO_EXPORT
-TEST_F( utglTF2ImportExport, exportglTF2FromFileTest ) {
-    EXPECT_TRUE( exporterTest() );
-}
-#endif // ASSIMP_BUILD_NO_EXPORT

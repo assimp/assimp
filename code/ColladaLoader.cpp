41724ace2d0fdba04330b95d4018b499a09ea73c
Turo Lamminen <turotl@gmail.com>
2017-09-18 14:10:58 +0300

    Collada: Silence uninitialized variable warning
    This is a false positive. Value of 'method' is only used if 'targetMeshes'
    contains something and all paths through the first loop which add stuff
    to 'targetMeshes' also set 'method'.

--- code/ColladaLoader.cpp
+++ code/ColladaLoader.cpp
@@ -674,7 +674,7 @@ aiMesh* ColladaLoader::CreateMesh( const ColladaParser& pParser, const Collada::
     // create morph target meshes if any
     std::vector<aiMesh*> targetMeshes;
     std::vector<float> targetWeights;
-    Collada::MorphMethod method;
+    Collada::MorphMethod method = Collada::Normalized;
 
     for(std::map<std::string, Collada::Controller>::const_iterator it = pParser.mControllerLibrary.begin();
         it != pParser.mControllerLibrary.end(); it++)

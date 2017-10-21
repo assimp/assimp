cc8374dd80821a423c41aa1ff85c98d12ca8e015
Alexandre Avenel <avenel.alexandre@gmail.com>
2017-10-21 20:36:43 +0200

    Return exception when obj file contains invalid face indice

--- code/ObjFileParser.cpp
+++ code/ObjFileParser.cpp
@@ -475,7 +475,11 @@ void ObjFileParser::getFace( aiPrimitiveType type ) {
                 } else {
                     reportErrorTokenInFace();
                 }
+            } else {
+                //On error, std::atoi will return 0 which is not a valid value
+                throw DeadlyImportError("OBJ: Invalid face indice");
             }
+
         }
         m_DataIt += iStep;
     }

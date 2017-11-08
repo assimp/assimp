cdfd4b9702ebf053d00321fe5b47ab8a35695866
Kim Kulling <kim.kulling@googlemail.com>
2017-11-08 20:44:53 +0100

    closes https://github.com/assimp/assimp/issues/1315: check in exporting against out-of-bounds-access .

--- code/ObjExporter.cpp
+++ code/ObjExporter.cpp
@@ -258,7 +258,6 @@ void ObjExporter::WriteMaterialFile()
     }
 }
 
-// ------------------------------------------------------------------------------------------------
 void ObjExporter::WriteGeometryFile(bool noMtl) {
     WriteHeader(mOutput);
     if (!noMtl)
@@ -280,8 +279,10 @@ void ObjExporter::WriteGeometryFile(bool noMtl) {
         mOutput << "# " << vp.size() << " vertex positions and colors" << endl;
         size_t colIdx = 0;
         for ( const aiVector3D& v : vp ) {
-            mOutput << "v  " << v.x << " " << v.y << " " << v.z << " " << vc[ colIdx ].r << " " << vc[ colIdx ].g << " " << vc[ colIdx ].b << endl;
-            colIdx++;
+            if ( colIdx < vc.size() ) {
+                mOutput << "v  " << v.x << " " << v.y << " " << v.z << " " << vc[ colIdx ].r << " " << vc[ colIdx ].g << " " << vc[ colIdx ].b << endl;
+            }
+            ++colIdx;
         }
     }
     mOutput << endl;

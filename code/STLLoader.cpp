5822d1920e83b08be6cf774e08affe5da967f880
Kim Kulling <kimkulling@users.noreply.github.com>
2017-11-30 09:22:55 +0100

    Update STLLoader.cpp
    Retrigger travis + appveyor.

--- code/STLLoader.cpp
+++ code/STLLoader.cpp
@@ -58,6 +58,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 using namespace Assimp;
 
 namespace {
+    
 static const aiImporterDesc desc = {
     "Stereolithography (STL) Importer",
     "",

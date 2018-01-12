4cb4d59682e4136e27760d2b5427617e9d09e3f5
BuildTools <unconfigured@null.spigotmc.org>
2018-01-11 16:13:13 -0800

    revert problematic unit test models to last upstream commits

--- test/unit/utBlendImportAreaLight.cpp
+++ test/unit/utBlendImportAreaLight.cpp
@@ -46,7 +46,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-#include <iostream>
 
 class BlendImportAreaLight : public ::testing::Test {
 public:
@@ -70,7 +69,6 @@ protected:
 TEST_F(BlendImportAreaLight, testImportLight)
 {
     const aiScene* pTest = im->ReadFile(ASSIMP_TEST_MODELS_DIR "/BLEND/AreaLight_269.blend", aiProcess_ValidateDataStructure);
-    if (!pTest) std::cerr << im->GetErrorString() << '\n'; // temporary, for pull req. diagnostics
     ASSERT_TRUE(pTest != NULL);
     ASSERT_TRUE(pTest->HasLights());
 

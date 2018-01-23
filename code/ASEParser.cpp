1b28124f21b8b81fa6f0df0a7fd094b96c4bf8a9
Turo Lamminen <turotl@gmail.com>
2018-01-23 19:54:16 +0200

    ASE: Pass a default material name when resizing submaterials buffer

--- code/ASEParser.cpp
+++ code/ASEParser.cpp
@@ -706,7 +706,7 @@ void Parser::ParseLV2MaterialBlock(ASE::Material& mat)
                 ParseLV4MeshLong(iNumSubMaterials);
 
                 // allocate enough storage
-                mat.avSubMaterials.resize(iNumSubMaterials);
+                mat.avSubMaterials.resize(iNumSubMaterials, Material("INVALID SUBMATERIAL"));
             }
             // submaterial chunks
             if (TokenMatch(filePtr,"SUBMATERIAL",11))

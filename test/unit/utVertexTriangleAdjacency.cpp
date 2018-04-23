43dedb6e0980f779ce61240e17d1df064cac65f7
Sergio Acereda <sacereda@users.noreply.github.com>
2018-04-23 12:23:12 +0200

    unit test: missing mNumIndices

--- test/unit/utVertexTriangleAdjacency.cpp
+++ test/unit/utVertexTriangleAdjacency.cpp
@@ -101,8 +101,11 @@ TEST_F(VTAdjacencyTest, smallDataSet)
 
     mesh.mFaces = new aiFace[3];
     mesh.mFaces[0].mIndices = new unsigned int[3];
+    mesh.mFaces[0].mNumIndices = 3;
     mesh.mFaces[1].mIndices = new unsigned int[3];
+    mesh.mFaces[1].mNumIndices = 3;
     mesh.mFaces[2].mIndices = new unsigned int[3];
+    mesh.mFaces[2].mNumIndices = 3;
 
     mesh.mFaces[0].mIndices[0] = 1;
     mesh.mFaces[0].mIndices[1] = 3;

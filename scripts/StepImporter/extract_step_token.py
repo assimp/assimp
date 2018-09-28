42402b2cf338cd200c5a6d9993daef2eb7b564cb
Kim Kulling <kim.kulling@googlemail.com>
2018-09-28 20:10:46 +0200

    Generate step-entity list.

--- scripts/StepImporter/extract_step_token.py
+++ scripts/StepImporter/extract_step_token.py
@@ -1,10 +1,20 @@
+import sys
 
+Entity_token = "ENTITY"
 token = []
 file = open(sys.argv[1])
 output = open("step_entitylist.txt", "a")
 lines = file.readlines()
 for line in lines:
-    pos = line.find("ENTITY")
+    pos = line.find(Entity_token)
     if pos != -1:
-        
+        token = line.split(" ")
+        if len(token) > 1:
+            name = token[1]
+            print( "Writing entity " + name)
+            output.write(name)
+output.close()
+file.close()
+
+
     

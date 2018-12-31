c63b1a1fd539ed6bd2024e5d0867f4edbc0a4d61
Kim Kulling <kim.kulling@googlemail.com>
2018-12-31 14:47:54 +0100

    fix review finding: dereference after null-check.

--- code/Q3BSPFileImporter.cpp
+++ code/Q3BSPFileImporter.cpp
@@ -400,6 +400,7 @@ void Q3BSPFileImporter::createTriangleTopology( const Q3BSP::Q3BSPModel *pModel,
             if (nullptr != m_pCurrentFace) {
                 m_pCurrentFace->mNumIndices = 3;
                 m_pCurrentFace->mIndices = new unsigned int[3];
+                m_pCurrentFace->mIndices[ idx ] = vertIdx;
             }
         }
 
@@ -409,9 +410,7 @@ void Q3BSPFileImporter::createTriangleTopology( const Q3BSP::Q3BSPModel *pModel,
         pMesh->mTextureCoords[ 0 ][ vertIdx ].Set( pVertex->vTexCoord.x, pVertex->vTexCoord.y, 0.0f );
         pMesh->mTextureCoords[ 1 ][ vertIdx ].Set( pVertex->vLightmap.x, pVertex->vLightmap.y, 0.0f );
 
-        m_pCurrentFace->mIndices[ idx ] = vertIdx;
         vertIdx++;
-
         idx++;
     }
 }

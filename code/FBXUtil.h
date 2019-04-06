ddfc140fc3e0b31dfcac5e4df5397ce48ff538eb
Matias Lavik <matias.lavik@hotmail.com>
2019-04-07 00:02:22 +0200

    Added <stdint.h>-include

--- code/FBXUtil.h
+++ code/FBXUtil.h
@@ -48,6 +48,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "FBXCompileConfig.h"
 #include "FBXTokenizer.h"
+#include <stdint.h>
 
 namespace Assimp {
 namespace FBX {

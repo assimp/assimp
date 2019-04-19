3914a3ac008abd7d27ca265d4f7d5834f94fd1fd
Jeremy Sigrist <jsigrist@esri.com>
2019-04-19 12:24:11 -0700

    build: Disable unused file formats

--- assimp.lua
+++ assimp.lua
@@ -19,33 +19,69 @@ project "assimp"
     "NoPCH",
   }
 
+  -- The only formats we want are 3DS, DAE, OBJ, GLTF, FBX
   defines {
     "ASSIMP_BUILD_NO_OWN_ZLIB",
     "ASSIMP_BUILD_NO_EXPORT",
-    "ASSIMP_BUILD_NO_IFC_IMPORTER",
+    "ASSIMP_BUILD_NO_3D_IMPORTER",
+    "ASSIMP_BUILD_NO_3MF_IMPORTER",
+    "ASSIMP_BUILD_NO_AC_IMPORTER",
+    "ASSIMP_BUILD_NO_AMF_IMPORTER",
+    "ASSIMP_BUILD_NO_ASE_IMPORTER",
+    "ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+    "ASSIMP_BUILD_NO_B3D_IMPORTER",
+    "ASSIMP_BUILD_NO_BASE_IMPORTER",
+    "ASSIMP_BUILD_NO_BLEND_IMPORTER",
+    "ASSIMP_BUILD_NO_BVH_IMPORTER",
     "ASSIMP_BUILD_NO_C4D_IMPORTER",
-    "ASSIMP_BUILD_NO_GLTF_IMPORTER",
+    "ASSIMP_BUILD_NO_CSM_IMPORTER",
+    "ASSIMP_BUILD_NO_DXF_IMPORTER",
+    "ASSIMP_BUILD_NO_HMP_IMPORTER",
+    "ASSIMP_BUILD_NO_IFC_IMPORTER",
+    "ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+    "ASSIMP_BUILD_NO_IRR_IMPORTER",
+    "ASSIMP_BUILD_NO_LWO_IMPORTER",
+    "ASSIMP_BUILD_NO_LWS_IMPORTER",
+    "ASSIMP_BUILD_NO_MD2_IMPORTER",
+    "ASSIMP_BUILD_NO_MD3_IMPORTER",
+    "ASSIMP_BUILD_NO_MD5_IMPORTER",
+    "ASSIMP_BUILD_NO_MDC_IMPORTER",
+    "ASSIMP_BUILD_NO_MDL_IMPORTER",
+    "ASSIMP_BUILD_NO_MMD_IMPORTER",
+    "ASSIMP_BUILD_NO_MS3D_IMPORTER",
+    "ASSIMP_BUILD_NO_NDO_IMPORTER",
+    "ASSIMP_BUILD_NO_NFF_IMPORTER",
+    "ASSIMP_BUILD_NO_OFF_IMPORTER",
+    "ASSIMP_BUILD_NO_OGRE_IMPORTER",
     "ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+    "ASSIMP_BUILD_NO_PLY_IMPORTER",
+    "ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+    "ASSIMP_BUILD_NO_Q3D_IMPORTER",
+    "ASSIMP_BUILD_NO_RAW_IMPORTER",
+    "ASSIMP_BUILD_NO_SIB_IMPORTER",
+    "ASSIMP_BUILD_NO_SMD_IMPORTER",
+    "ASSIMP_BUILD_NO_STL_IMPORTER",
+    "ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+    "ASSIMP_BUILD_NO_X3D_IMPORTER",
+    "ASSIMP_BUILD_NO_XGL_IMPORTER",
   }
 
   files {
     "code/**.cpp",
     "code/**.h",
     "code/**.hpp",
+    "contrib/irrXML/irrXML.h", -- Required by Collada (DAE) reader
     "contrib/irrXML/irrXML.cpp",
-    "contrib/irrXML/irrXML.h",
+    "contrib/rapidjson/include/**.h",
   }
 
   excludes {
-    "code/C4D*", -- Only supported on MSVC
-    "code/OpenGEX*", -- Requires OpenDDLParser
-    "code/glTF2*", -- Requires rapidjson
-    "code/glTF*",  -- Requires rapidjson
   }
 
   includedirs {
     "include",
     "contrib/irrXML",
+    "contrib/rapidjson/include",
     prjDir,
     prjDir .. "/../boost",
     prjDir .. "/../zlib",

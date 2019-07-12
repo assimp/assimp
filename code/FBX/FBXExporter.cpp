f9f408690e1081fbf338e6c39e4ff93c7b5158cb
thomasbiang <weisi.dai@gmail.com>
2019-07-11 17:00:47 -0700

    use ExportProperties to customize bind pose epsilon

--- code/FBX/FBXExporter.cpp
+++ code/FBX/FBXExporter.cpp
@@ -67,6 +67,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vector>
 #include <array>
 #include <unordered_set>
+#include <iostream>
 
 // RESOURCES:
 // https://code.blender.org/2013/08/fbx-binary-file-format-specification/

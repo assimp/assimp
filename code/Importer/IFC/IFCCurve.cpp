116dcce0ba357163aa907dbcb8c4c64177085f1b
escherstair <ernestviga@gmail.com>
2019-09-18 08:04:46 +0200

    init member in itilializer list

--- code/Importer/IFC/IFCCurve.cpp
+++ code/Importer/IFC/IFCCurve.cpp
@@ -311,10 +311,9 @@ class TrimmedCurve : public BoundedCurve {
 public:
     // --------------------------------------------------
     TrimmedCurve(const Schema_2x3::IfcTrimmedCurve& entity, ConversionData& conv)
-        : BoundedCurve(entity,conv)
+        : BoundedCurve(entity,conv),
+          base(std::shared_ptr<const Curve>(Curve::Convert(entity.BasisCurve,conv)))
     {
-        base = std::shared_ptr<const Curve>(Curve::Convert(entity.BasisCurve,conv));
-
         typedef std::shared_ptr<const STEP::EXPRESS::DataType> Entry;
 
         // for some reason, trimmed curves can either specify a parametric value

7a8b7ba88d6d84dde7fd43419ac2b022c9887856
Theak <theakman2@hotmail.com>
2019-09-29 10:09:53 +0100

    aiAnimation::mTicksPerSecond for gltf2 imports - should always be 1000.0

--- code/glTF2/glTF2Importer.cpp
+++ code/glTF2/glTF2Importer.cpp
@@ -1176,7 +1176,7 @@ void glTF2Importer::ImportAnimations(glTF2::Asset& r)
             }
         }
         ai_anim->mDuration = maxDuration;
-        ai_anim->mTicksPerSecond = 0.001;
+        ai_anim->mTicksPerSecond = 1000.0;
 
         mScene->mAnimations[i] = ai_anim;
     }

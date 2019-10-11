789a2bdd3602839235163189595c1b1263ae8a31
kkulling <kim.kulling@slm-solutions.com>
2019-10-11 09:47:31 +0200

    Move stuff into def.h

--- include/assimp/Macros.h
+++ include/assimp/Macros.h
@@ -39,11 +39,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ---------------------------------------------------------------------------
 */
 
-/* Helper macro to set a pointer to NULL in debug builds
- */
-#if (defined ASSIMP_BUILD_DEBUG)
-#   define AI_DEBUG_INVALIDATE_PTR(x) x = NULL;
-#else
-#   define AI_DEBUG_INVALIDATE_PTR(x)
-#endif
+#pragma once
+
 

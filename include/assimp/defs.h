ddc6f3407c4d1d50f83d35ecf69b3c761cc9d17b
rickomax <rickomax@hotmail.com>
2019-12-25 22:14:52 -0300

    Update defs.h

--- include/assimp/defs.h
+++ include/assimp/defs.h
@@ -127,9 +127,15 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * FIXTEXTUREPATHS
  * GENBOUNDINGBOXES */
 //////////////////////////////////////////////////////////////////////////
+
+#ifdef _WIN32
+#   define ASSIMP_CALLCONV __stdcall
+#else
+#   define ASSIMP_CALLCONV
+#endif
+
 #ifdef _WIN32
 #   undef ASSIMP_API
-#   define ASSIMP_CALLCONV __stdcall //rickomax: stdcall to use in C# interop calls under windows
     //////////////////////////////////////////////////////////////////////////
     /* Define 'ASSIMP_BUILD_DLL_EXPORT' to build a DLL of the library */
     //////////////////////////////////////////////////////////////////////////
@@ -153,7 +159,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     /* Do nothing, the relevant defines are all in AssimpSwigPort.i */
 
 #else
-#   define ASSIMP_CALLCONV  //rickomax: no callconv to use in C# interop calls under other platforms
 #   define ASSIMP_API __attribute__ ((visibility("default")))
 #   define ASSIMP_API_WINONLY
 #endif

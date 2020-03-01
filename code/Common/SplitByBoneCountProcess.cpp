2917ab04b728be481c905b96b7c1fb44ea656141
iamAdrianIusca <iamAdrianIusca@gmail.com>
2020-03-01 11:22:44 +0200

    fix

--- code/Common/SplitByBoneCountProcess.cpp
+++ code/Common/SplitByBoneCountProcess.cpp
@@ -165,7 +165,7 @@ void SplitByBoneCountProcess::SplitMesh( const aiMesh* pMesh, std::vector<aiMesh
     {
         const aiBone* bone = pMesh->mBones[a];
         for( unsigned int b = 0; b < bone->mNumWeights; ++b)
-	        vertexBones[ bone->mWeights[b].mVertexId ].push_back( BoneWeight( a, bone->mWeights[b].mWeight));
+			vertexBones[ bone->mWeights[b].mVertexId ].push_back( BoneWeight( a, bone->mWeights[b].mWeight));
     }
 
     unsigned int numFacesHandled = 0;

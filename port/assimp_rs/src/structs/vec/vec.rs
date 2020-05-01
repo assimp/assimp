778c3afbbb052bea25d6eaaccba1d663432b0b27
David Golembiowski <dmgolembiowski@gmail.com>
2020-05-01 18:11:44 -0400

    Rust's import system dictates that we must make directories rather than use individual files to provide a clean public interface

--- port/assimp_rs/src/structs/vec/vec.rs
+++ port/assimp_rs/src/structs/vec/vec.rs
@@ -9,6 +9,13 @@ struct Vector3d {
     z: f32
 }
 
+struct Vector4d {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32
+}
+
 impl Vector2d {
     pub fn new(x_f32: f32, y_f32: f32) -> Vector2d {
         Vector2d {
@@ -27,3 +34,15 @@ impl Vector3d {
         }
     }
 }
+
+impl Vector4d {
+    pub fn new(x_f32: f32, y_f32: f32, z_f32: f32, w_f32: f32) -> Vector4d {
+        Vector4d {
+            x: x_f32,
+            y: y_f32,
+            z: z_f32,
+            w: w_f32
+        }
+    }
+}
+

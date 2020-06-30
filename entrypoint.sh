8bdfa7041b5d666a8d76d6ee06d7a07357df1b93
Bernhard Reitinger <br@rexos.org>
2020-06-30 09:09:11 +0200

    Fix entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ cd /code
 mkdir -p build
 cd build
 rm -f $PACKAGE $TARGET
-cmake ..
+cmake -DASSIMP_BUILD_ZLIB=ON ..
 make -j4
 tar cf $TARGET \
     bin/assimp \
     bin/libassimp.so \
     bin/libassimp.so.5 \
     bin/libassimp.so.5.0.1 \
-    bin/libzlib.so \
-    bin/libzlib.so.1
+    # bin/libzlib.so \
+    # bin/libzlib.so.1
 gzip $TARGET
 rm -f $TARGET

cc2613f2644fd7ccb8730be871aba1de25128d49
Joshua Hyatt <joshua.n.hyatt@gmail.com>
2020-08-29 23:06:33 -0600

    Replace unique_ptr with raw pointer

--- code/AssetLib/FBX/FBXImporter.cpp
+++ code/AssetLib/FBX/FBXImporter.cpp
@@ -141,7 +141,7 @@ void FBXImporter::SetupProperties(const Importer *pImp) {
 // ------------------------------------------------------------------------------------------------
 // Imports the given file into the given scene structure.
 void FBXImporter::InternReadFile(const std::string &pFile, aiScene *pScene, IOSystem *pIOHandler) {
-	std::unique_ptr<IOStream> stream(pIOHandler->Open(pFile, "rb"));
+	IOStream* stream = pIOHandler->Open(pFile, "rb");
 	if (!stream) {
 		ThrowException("Could not open file for reading");
 	}

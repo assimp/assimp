9d296f39433abe33821210e2fe6c663e830d70f5
Kim Kulling <kimkulling@users.noreply.github.com>
2020-10-01 22:02:03 +0200

    export pugixml symbold in assimp

--- contrib/pugixml/src/pugiconfig.hpp
+++ contrib/pugixml/src/pugiconfig.hpp
@@ -30,8 +30,8 @@
 // #define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
-// #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
-// #define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
+#define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
+#define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
 // #define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
 // In absence of PUGIXML_CLASS/PUGIXML_FUNCTION definitions PUGIXML_API is used instead
 

9303fb31f65e6e91503ffd74a325f2126a34533f
Kim Kulling <kimkulling@users.noreply.github.com>
2020-10-02 11:30:37 +0200

    Enable export only for windows

--- contrib/pugixml/src/pugiconfig.hpp
+++ contrib/pugixml/src/pugiconfig.hpp
@@ -30,8 +30,10 @@
 // #define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
+#ifdef _WIN32
 #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
 #define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
+#endif
 // #define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
 // In absence of PUGIXML_CLASS/PUGIXML_FUNCTION definitions PUGIXML_API is used instead
 

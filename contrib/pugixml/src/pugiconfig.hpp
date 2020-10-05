23f144a7b78a4e7579fdefb1e6f7d2d71fb4d712
Kim Kulling <kim.kulling@googlemail.com>
2020-10-05 18:44:59 +0200

    pugixml: use pugixml in headeronly mode.

--- contrib/pugixml/src/pugiconfig.hpp
+++ contrib/pugixml/src/pugiconfig.hpp
@@ -30,10 +30,10 @@
 // #define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
-#ifdef _WIN32
-#define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
-#define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
-#endif
+//#ifdef _WIN32
+//#define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
+//#define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
+//#endif
 // #define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
 // In absence of PUGIXML_CLASS/PUGIXML_FUNCTION definitions PUGIXML_API is used instead
 
@@ -43,10 +43,10 @@
 // #define PUGIXML_MEMORY_XPATH_PAGE_SIZE 4096
 
 // Uncomment this to switch to header-only version
-// #define PUGIXML_HEADER_ONLY
+#define PUGIXML_HEADER_ONLY
 
 // Uncomment this to enable long long support
-// #define PUGIXML_HAS_LONG_LONG
+//#define PUGIXML_HAS_LONG_LONG
 
 #endif
 

c0c7e6a0de94c450275775d8a94582f7a6457871
Urs Hanselmann <git@hanselmann.io>
2021-03-12 14:48:38 +0100

    trim uvIndices to fix import of Cheetah3D generated fbx files

--- code/AssetLib/FBX/FBXMeshGeometry.cpp
+++ code/AssetLib/FBX/FBXMeshGeometry.cpp
@@ -508,11 +508,17 @@ void ResolveVertexDataArray(std::vector<T>& data_out, const Scope& source,
         std::vector<int> uvIndices;
         ParseVectorDataArray(uvIndices,GetRequiredElement(source,indexDataElementName));
 
-        // if (uvIndices.size() != vertex_count) {
-        //     FBXImporter::LogError(Formatter::format("length of input data unexpected for ByPolygonVertex mapping: ")
-        //                           << uvIndices.size() << ", expected " << vertex_count);
-        //     return;
-        // }
+        if (uvIndices.size() > vertex_count) {
+            FBXImporter::LogWarn(Formatter::format("trimming length of input array for ByPolygonVertex mapping: ")
+                                          << uvIndices.size() << ", expected " << vertex_count);
+            uvIndices.resize(vertex_count);
+        }
+
+        if (uvIndices.size() != vertex_count) {
+            FBXImporter::LogError(Formatter::format("length of input data unexpected for ByPolygonVertex mapping: ")
+                                  << uvIndices.size() << ", expected " << vertex_count);
+            return;
+        }
 
         data_out.resize(vertex_count);
 

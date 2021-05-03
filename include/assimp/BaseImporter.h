b87f67db4c10d568637d19a35bd986db49e92ceb
Kim Kulling <kimkulling@users.noreply.github.com>
2021-05-03 20:49:14 +0200

    Add missing include

--- include/assimp/BaseImporter.h
+++ include/assimp/BaseImporter.h
@@ -58,6 +58,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <set>
 #include <vector>
 #include <memory>
+#include <iterator>
 
 struct aiScene;
 struct aiImporterDesc;

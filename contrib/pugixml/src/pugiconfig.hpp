bf0f8d4c1b2f53a457b4e1834abf7f841ef30300
Kim Kulling <kimkulling@users.noreply.github.com>
2021-06-03 23:29:53 +0200

    Update pugiconfig.hpp

--- contrib/pugixml/src/pugiconfig.hpp
+++ contrib/pugixml/src/pugiconfig.hpp
@@ -44,7 +44,7 @@
 // #define PUGIXML_XPATH_DEPTH_LIMIT 1024
 
 // Uncomment this to switch to header-only version
-// #define PUGIXML_HEADER_ONLY
+#define PUGIXML_HEADER_ONLY
 
 // Uncomment this to enable long long support
 // #define PUGIXML_HAS_LONG_LONG

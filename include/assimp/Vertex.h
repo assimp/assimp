6498b50e1431f30d62462bf680e1c4baa425c929
Max Vollmer (Microsoft Havok) <60260460+ms-maxvollmer@users.noreply.github.com>
2021-11-26 12:51:30 +0000

    Check that positions exist before accessing them

--- include/assimp/Vertex.h
+++ include/assimp/Vertex.h
@@ -135,7 +135,9 @@ public:
     /** Extract a particular vertex from a anim mesh and interleave all components */
     explicit Vertex(const aiAnimMesh* msh, unsigned int idx) {
         ai_assert(idx < msh->mNumVertices);
-        position = msh->mVertices[idx];
+        if (msh->HasPositions()) {
+            position = msh->mVertices[idx];
+        }
 
         if (msh->HasNormals()) {
             normal = msh->mNormals[idx];

43763f53d6d794c524a4f9ba47809a83b8997c04
Kim Kulling <kim.kulling@googlemail.com>
2022-01-19 22:00:53 +0100

    Fix compile error: missing var

--- code/AssetLib/Blender/BlenderLoader.cpp
+++ code/AssetLib/Blender/BlenderLoader.cpp
@@ -114,12 +114,14 @@ BlenderImporter::~BlenderImporter() {
 }
 
 static const char * const Tokens[] = { "BLENDER" };
-static const char * const TokensForSearch[] = { "blender" };
+//static const char * const TokensForSearch[] = { "blender" };
 
 // ------------------------------------------------------------------------------------------------
 // Returns whether the class can handle the format of the given file.
 bool BlenderImporter::CanRead(const std::string &pFile, IOSystem *pIOHandler, bool /*checkSig*/) const {
     // note: this won't catch compressed files
+    static const char *tokens[] = { "<BLENDER", "blender" };
+
     return SearchFileHeaderForToken(pIOHandler, pFile, tokens, AI_COUNT_OF(tokens));
 }
 

5c438963c0aa6991fe88fb5f50f2381999d8f7ac
Kim Kulling <kimkulling@users.noreply.github.com>
2022-04-05 19:59:36 +0200

    Disable correct compiler-warning: unused-function.

--- code/Common/StbCommon.h
+++ code/Common/StbCommon.h
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma warning(disable : 4505)
 #else
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-function"
 #endif
 
 #define STB_IMAGE_STATIC

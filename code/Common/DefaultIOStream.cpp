55296f08d0771d760153e68d6b2ef1f85b456988
Thaddeus Schelp <thadd.schelp@gmail.com>
2022-06-16 15:56:59 -0400

    Added _WIN32

--- code/Common/DefaultIOStream.cpp
+++ code/Common/DefaultIOStream.cpp
@@ -156,7 +156,7 @@ size_t DefaultIOStream::FileSize() const {
         if (0 != err)
             return 0;
         mCachedSize = (size_t)(fileStat.st_size);
-#elif defined __GNUC__ || defined __APPLE__ || defined __MACH__ || defined __FreeBSD__
+#elif defined __GNUC__ || defined __APPLE__ || defined __MACH__ || defined __FreeBSD__ || defined _WIN32
         struct stat fileStat;
         int err = stat(mFilename.c_str(), &fileStat);
         if (0 != err)

6a1a7c43cc7cf916b095d2f3a5d4e0d1cd057b1f
Kim Kulling <kimkulling@users.noreply.github.com>
2022-09-08 19:32:42 +0200

    Update utMaybe.cpp

--- test/unit/Common/utMaybe.cpp
+++ test/unit/Common/utMaybe.cpp
@@ -40,7 +40,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "UnitTestPCH.h"
-#include "COmmon/Maybe.h"
+#include "Common/Maybe.h"
 
 using namespace Assimp;
 

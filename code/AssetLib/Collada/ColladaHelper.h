e893248e7c88b7c2133ee2201d89d729091bd545
Andy Maloney <asmaloney@gmail.com>
2023-01-01 13:27:16 -0500

    Fix several spelling mistakes & a comment

--- code/AssetLib/Collada/ColladaHelper.h
+++ code/AssetLib/Collada/ColladaHelper.h
@@ -666,7 +666,7 @@ struct ChannelEntry {
     const Collada::Accessor *mTimeAccessor; ///> Collada accessor to the time values
     const Collada::Data *mTimeData; ///> Source data array for the time values
     const Collada::Accessor *mValueAccessor; ///> Collada accessor to the key value values
-    const Collada::Data *mValueData; ///> Source datat array for the key value values
+    const Collada::Data *mValueData; ///> Source data array for the key value values
 
     ChannelEntry() :
             mChannel(),

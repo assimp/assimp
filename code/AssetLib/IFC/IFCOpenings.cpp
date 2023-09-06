b5c4414dd3b94105d7382ec8e26b6a4bc9ea0483
Kim Kulling <kim.kullingk@draeger.com>
2023-09-06 15:08:54 +0200

    Fix invalid use of hunter enabled macro.

--- code/AssetLib/IFC/IFCOpenings.cpp
+++ code/AssetLib/IFC/IFCOpenings.cpp
@@ -48,13 +48,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "Common/PolyTools.h"
 #include "PostProcessing/ProcessHelper.h"
 
-#ifdef ASSIMP_USE_HUNTER
-#  include <poly2tri/poly2tri.h>
-#  include <polyclipping/clipper.hpp>
-#else
+//#ifdef ASSIMP_USE_HUNTER
+//#  include <poly2tri/poly2tri.h>
+//#  include <polyclipping/clipper.hpp>
+//#else
 #  include "contrib/poly2tri/poly2tri/poly2tri.h"
 #  include "contrib/clipper/clipper.hpp"
-#endif // ASSIMP_USE_HUNTER
+//#endif // ASSIMP_USE_HUNTER
 
 #include <deque>
 #include <forward_list>

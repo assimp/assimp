a37c75f026edcf8a7f7c3a4d1da388e3b45b5b31
IOhannes m zmölnig <zmoelnig@iem.at>
2023-11-06 13:32:54 +0100

    Drop disabled zlib.h inclusion stubs

--- code/AssetLib/FBX/FBXParser.cpp
+++ code/AssetLib/FBX/FBXParser.cpp
@@ -45,12 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef ASSIMP_BUILD_NO_FBX_IMPORTER
 
-//#ifdef ASSIMP_BUILD_NO_OWN_ZLIB
 #include "Common/Compression.h"
-//#   include <zlib.h>
-//#else
-//#   include "../contrib/zlib/zlib.h"
-//#endif
 
 #include "FBXTokenizer.h"
 #include "FBXParser.h"

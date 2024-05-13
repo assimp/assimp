fef9fbea7cbed02272f2cfb21578b10f4339e45c
Kim Kulling <kim.kulling@draeger.com>
2024-05-13 08:42:28 +0200

    Add checks for invalid buffer ans size

--- code/Common/Assimp.cpp
+++ code/Common/Assimp.cpp
@@ -233,8 +233,13 @@ const aiScene *aiImportFileFromMemoryWithProperties(
         unsigned int pFlags,
         const char *pHint,
         const aiPropertyStore *props) {
-    ai_assert(nullptr != pBuffer);
-    ai_assert(0 != pLength);
+    if (pBuffer == nullptr) {
+        return nullptr;
+    }
+
+    if (pLength == 0u) {
+        return nullptr;
+    }
 
     const aiScene *scene = nullptr;
     ASSIMP_BEGIN_EXCEPTION_REGION();

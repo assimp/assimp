3ff60401a7172514ec026f6746b55ce766ad8433
Kim Kulling <kimkulling@users.noreply.github.com>
2024-05-23 13:39:04 +0200

    Check for double defined macro

--- contrib/zip/src/miniz.h
+++ contrib/zip/src/miniz.h
@@ -4954,7 +4954,9 @@ extern "C" {
 
 #if defined(_MSC_VER) || defined(__MINGW64__)
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 static WCHAR *mz_utf8z_to_widechar(const char *str) {

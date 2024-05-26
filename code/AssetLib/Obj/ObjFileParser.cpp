98a18dbd374e29ad2f2101f308ea2afe70cc363c
Kim Kulling <kimkulling@users.noreply.github.com>
2024-05-26 18:19:56 +0200

    Update ObjFileParser.cpp
    Ensure not checking empty strings

--- code/AssetLib/Obj/ObjFileParser.cpp
+++ code/AssetLib/Obj/ObjFileParser.cpp
@@ -582,10 +582,12 @@ void ObjFileParser::getMaterialDesc() {
         skip = true;
     }
 
-    // If the current mesh has the same material, we simply ignore that 'usemtl' command
+    // If the current mesh has the same material, we will ignore that 'usemtl' command
     // There is no need to create another object or even mesh here
-    if (m_pModel->mCurrentMaterial && m_pModel->mCurrentMaterial->MaterialName == aiString(strName)) {
-        skip = true;
+    if (!skip) {
+        if (m_pModel->mCurrentMaterial && m_pModel->mCurrentMaterial->MaterialName == aiString(strName)) {
+            skip = true;
+        }
     }
 
     if (!skip) {

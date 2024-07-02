0eb085fc7232029caddfb1f13e367e760b0ad6be
Matthias Möller <m_moeller@live.de>
2024-07-02 21:54:18 +0200

    fixes some uninit bool loads (#5644)
    This commit fixes some bool loads which are not initialized. With ubsan and the "option -fsanitize=bool", this results in a runtime error during test execution.
    
    Co-authored-by: Kim Kulling <kimkulling@users.noreply.github.com>

--- test/unit/utMetadata.cpp
+++ test/unit/utMetadata.cpp
@@ -242,6 +242,22 @@ TEST_F( utMetadata, copy_test ) {
         EXPECT_EQ( i32v, v );
     }
 
+    // uint32_t test
+    {
+        uint32_t v = 0;
+        bool ok = copy.Get("uint32_t", v);
+        EXPECT_TRUE(ok);
+        EXPECT_EQ( ui32, v );
+    }
+
+    // int64_t test
+    {
+        int64_t v = -1;
+        bool ok = copy.Get("int64_t", v);
+        EXPECT_TRUE(ok);
+        EXPECT_EQ( i64, v );
+    }
+
     // uint64_t test
     {
         uint64_t v = 255;
@@ -264,14 +280,14 @@ TEST_F( utMetadata, copy_test ) {
         EXPECT_EQ( dv, v );
     }
 
-    // bool test
+    // string test
     {
         aiString v;
         EXPECT_TRUE( copy.Get( "aiString", v ) );
         EXPECT_EQ( strVal, v );
     }
 
-    // bool test
+    // vector test
     {
         aiVector3D v;
         EXPECT_TRUE( copy.Get( "aiVector3D", v ) );

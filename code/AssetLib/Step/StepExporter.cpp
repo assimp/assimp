9cf03b4ddcd6585669aad98d6226c213bcaa0e9e
Kim Kulling <kimkulling@users.noreply.github.com>
2024-07-18 23:54:31 +0200

    Introduce interpolation mode to vectro and quaternion keys (#5674)
    * Introduce interpolation mode to vectro and quaternion keys
    
    * Update USDLoader.cpp
    
    Fix review finding
    
    * Update USDLoader.h
    
    Fix review finding,

--- code/AssetLib/Step/StepExporter.cpp
+++ code/AssetLib/Step/StepExporter.cpp
@@ -4,7 +4,6 @@ Open Asset Import Library (assimp)
 
 Copyright (c) 2006-2024, assimp team
 
-
 All rights reserved.
 
 Redistribution and use of this software in source and binary forms,
@@ -37,10 +36,8 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-@author: Richard Steffen, 2015
 ----------------------------------------------------------------------
 */
-
 
 #ifndef ASSIMP_BUILD_NO_EXPORT
 #ifndef ASSIMP_BUILD_NO_STEP_EXPORTER

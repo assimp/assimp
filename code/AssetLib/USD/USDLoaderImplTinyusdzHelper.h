2cf754aa469b5665f8422728d5f155920c60938c
sacereda <27723692+sacereda@users.noreply.github.com>
2024-12-12 20:58:08 +0100

    Fixed warnings (#5903)
    * Fixed conversion warning when compiling without ASSIMP_DOUBLE_PRECISION. Fixed size() <> unsigned warnings
    
    * Fix: Review finding
    
    ---------
    
    Co-authored-by: Kim Kulling <kimkulling@users.noreply.github.com>

--- code/AssetLib/USD/USDLoaderImplTinyusdzHelper.h
+++ code/AssetLib/USD/USDLoaderImplTinyusdzHelper.h
@@ -60,22 +60,22 @@ template <typename T>
 aiMatrix4x4 tinyUsdzMat4ToAiMat4(const T matIn[4][4]) {
     static_assert(std::is_floating_point_v<T>, "Only floating-point types are allowed.");
     aiMatrix4x4 matOut;
-    matOut.a1 = matIn[0][0];
-    matOut.a2 = matIn[1][0];
-    matOut.a3 = matIn[2][0];
-    matOut.a4 = matIn[3][0];
-    matOut.b1 = matIn[0][1];
-    matOut.b2 = matIn[1][1];
-    matOut.b3 = matIn[2][1];
-    matOut.b4 = matIn[3][1];
-    matOut.c1 = matIn[0][2];
-    matOut.c2 = matIn[1][2];
-    matOut.c3 = matIn[2][2];
-    matOut.c4 = matIn[3][2];
-    matOut.d1 = matIn[0][3];
-    matOut.d2 = matIn[1][3];
-    matOut.d3 = matIn[2][3];
-    matOut.d4 = matIn[3][3];
+    matOut.a1 = ai_real(matIn[0][0]);
+    matOut.a2 = ai_real(matIn[1][0]);
+    matOut.a3 = ai_real(matIn[2][0]);
+    matOut.a4 = ai_real(matIn[3][0]);
+    matOut.b1 = ai_real(matIn[0][1]);
+    matOut.b2 = ai_real(matIn[1][1]);
+    matOut.b3 = ai_real(matIn[2][1]);
+    matOut.b4 = ai_real(matIn[3][1]);
+    matOut.c1 = ai_real(matIn[0][2]);
+    matOut.c2 = ai_real(matIn[1][2]);
+    matOut.c3 = ai_real(matIn[2][2]);
+    matOut.c4 = ai_real(matIn[3][2]);
+    matOut.d1 = ai_real(matIn[0][3]);
+    matOut.d2 = ai_real(matIn[1][3]);
+    matOut.d3 = ai_real(matIn[2][3]);
+    matOut.d4 = ai_real(matIn[3][3]);
     return matOut;
 }
 aiVector3D tinyUsdzScaleOrPosToAssimp(const std::array<float, 3> &scaleOrPosIn);

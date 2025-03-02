c54ceaab7c404500f412629159dc99556c300134
cielavenir <cielartisan@gmail.com>
2025-03-02 16:01:04 +0900

    Use std::copy to copy array

--- code/Common/SceneCombiner.cpp
+++ code/Common/SceneCombiner.cpp
@@ -985,7 +985,7 @@ inline void GetArrayCopy(Type *&dest, ai_uint num) {
     Type *old = dest;
 
     dest = new Type[num];
-    ::memcpy((void*)dest, old, sizeof(Type) * num);
+    std::copy(old, old+num, dest);
 }
 
 // ------------------------------------------------------------------------------------------------

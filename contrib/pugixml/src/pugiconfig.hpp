25227a6a5469c0518ed574ec96c8d1dad9d24ce1
Kim Kulling <kim.kulling@googlemail.com>
2025-06-07 16:35:06 +0200

    pugixml: upgrade to v1.15

--- contrib/pugixml/src/pugiconfig.hpp
+++ contrib/pugixml/src/pugiconfig.hpp
@@ -30,9 +30,9 @@
 // #define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
-// #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
-// #define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
-// #define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
+#define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
+//define PUGIXML_CLASS __declspec(dllimport) // to import all classes from DLL
+//#define PUGIXML_FUNCTION __fastcall // to set calling conventions to all public functions to fastcall
 // In absence of PUGIXML_CLASS/PUGIXML_FUNCTION definitions PUGIXML_API is used instead
 
 // Tune these constants to adjust memory-related behavior

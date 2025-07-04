35ed5e20b63df5fd5ff8567ddaa227ccd85ab360
Kim Kulling <kimkulling@users.noreply.github.com>
2025-07-04 14:09:30 +0200

    Update gzwrite.c

--- contrib/zlib/gzwrite.c
+++ contrib/zlib/gzwrite.c
@@ -5,7 +5,9 @@
 #ifdef _WIN32
 #   define _CRT_NONSTDC_NO_DEPRECATE
 #endif 
-
+#ifndef _CRT_SECURE_NO_WARNINGS
+#   define _CRT_SECURE_NO_WARNINGS
+#endif // _CRT_SECURE_NO_WARNINGS
 #include "gzguts.h"
 
 /* Local functions */

bec3e1a0897a7bf930f7ec0ca1dd26faa83258b2
Kim Kulling <kimkulling@users.noreply.github.com>
2025-11-11 15:35:25 +0100

    Refactor glTFCommon namespace structure
    Refactor namespace declarations for consistency.

--- code/AssetLib/glTFCommon/glTFCommon.cpp
+++ code/AssetLib/glTFCommon/glTFCommon.cpp
@@ -38,15 +38,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ----------------------------------------------------------------------
 */
-//#ifndef ASSIMP_BUILD_NO_GLTF_IMPORTER
-
 #include "AssetLib/glTFCommon/glTFCommon.h"
-
-namespace glTFCommon {
 
 using namespace glTFCommon::Util;
 
-namespace Util {
+namespace glTFCommon::Util {
 
 bool ParseDataURI(const char *const_uri, size_t uriLen, DataURI &out) {
     if (nullptr == const_uri) {
@@ -111,7 +107,4 @@ bool ParseDataURI(const char *const_uri, size_t uriLen, DataURI &out) {
     return true;
 }
 
-} // namespace Util
-} // namespace glTFCommon
-
-//#endif
+} // namespace glTFCommon::Uti

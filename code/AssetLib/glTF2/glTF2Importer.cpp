ec42a1ac8ea7bc77d7a535bd29020e5039705cf4
Orleonn <martianow.art2012@gmail.com>
2025-11-18 12:52:27 +0400

    Added missing scene extras properties for glTF2Importer

--- code/AssetLib/glTF2/glTF2Importer.cpp
+++ code/AssetLib/glTF2/glTF2Importer.cpp
@@ -1681,7 +1681,8 @@ void glTF2Importer::ImportCommonMetadata(glTF2::Asset &a) {
     const bool hasGenerator = !a.asset.generator.empty();
     const bool hasCopyright = !a.asset.copyright.empty();
     const bool hasSceneMetadata = a.scene->customExtensions;
-    if (hasVersion || hasGenerator || hasCopyright || hasSceneMetadata) {
+    const bool hasSceneExtras = a.scene->extras.HasExtras();
+    if (hasVersion || hasGenerator || hasCopyright || hasSceneMetadata || hasSceneExtras) {
         mScene->mMetaData = new aiMetadata;
         if (hasVersion) {
             mScene->mMetaData->Add(AI_METADATA_SOURCE_FORMAT_VERSION, aiString(a.asset.version));
@@ -1694,6 +1695,9 @@ void glTF2Importer::ImportCommonMetadata(glTF2::Asset &a) {
         }
         if (hasSceneMetadata) {
             ParseExtensions(mScene->mMetaData, a.scene->customExtensions);
+        }
+        if (hasSceneExtras) {
+            ParseExtras(mScene->mMetaData, a.scene->extras);
         }
     }
 }

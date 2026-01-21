1cd38c4a621e1bbc9bcd0e155b53f8c1b057d343
nickykitchingman <nickykit@hotmail.co.uk>
2026-01-21 13:24:21 +0000

    Fix some recently implemented comparisons of token strings. Previously, the `keyword` included the following space along with the token, which broke the string comparison using the equality operator.

--- code/AssetLib/Obj/ObjTools.h
+++ code/AssetLib/Obj/ObjTools.h
@@ -86,6 +86,23 @@ inline Char_T getNextWord(Char_T pBuffer, Char_T pEnd) {
 }
 
 /**
+ *  @brief  Returns pointer to end of the current token
+ *  @param[in] pBuffer  Pointer to data buffer
+ *  @param[in] pEnd     Pointer to end of buffer
+ *  @return Pointer to last character of current token
+ */
+template <class Char_T>
+inline Char_T getEndOfToken(Char_T pBuffer, Char_T pEnd) {
+    while (!isEndOfBuffer(pBuffer, pEnd)) {
+        if (IsSpaceOrNewLine(*pBuffer)) {
+            break;
+        }
+        ++pBuffer;
+    }
+    return pBuffer;
+}
+
+/**
  *  @brief  Returns pointer a next token
  *  @param[in] pBuffer  Pointer to data buffer
  *  @param[in] pEnd     Pointer to end of buffer
@@ -93,12 +110,7 @@ inline Char_T getNextWord(Char_T pBuffer, Char_T pEnd) {
  */
 template <class Char_T>
 inline Char_T getNextToken(Char_T pBuffer, Char_T pEnd) {
-    while (!isEndOfBuffer(pBuffer, pEnd)) {
-        if (IsSpaceOrNewLine(*pBuffer)) {
-            break;
-        }
-        ++pBuffer;
-    }
+    pBuffer = getEndOfToken(pBuffer, pEnd);
     return getNextWord(pBuffer, pEnd);
 }
 

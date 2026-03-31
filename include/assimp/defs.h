41d7c8cd5c7bc5811e0d80044ffbc9b2adf8df65
Kim Kulling <kimkulling@users.noreply.github.com>
2026-03-31 16:37:25 +0200

    Update defs.h
    Would this work as well?

--- include/assimp/defs.h
+++ include/assimp/defs.h
@@ -299,7 +299,7 @@ typedef unsigned int ai_uint;
 
 /* Numerical limits */
 #ifdef __cplusplus
-inline constexpr ai_real ai_epsilon = (ai_real) 1e-6;
+static constexpr ai_real ai_epsilon = (ai_real) 1e-6;
 #else
 #  define ai_epsilon ((ai_real)1e-6)
 #endif

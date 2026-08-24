394041fff1d7b9e07a55102d7166c90d9af87a7f
Turo Lamminen <turol@users.noreply.github.com>
2026-08-24 09:58:34 +0300

    Miscellaneous fixes (#6801)
    * Add missing include
    
    * Fix uninitialized member variables in Blender statistics
    
    * Remove unnecessary temporary variable
    
    * Add more RAW import unit tests
    
    Some are disabled because they're currently broken
    
    * Add braces to else
    
    * Split a compound assignment
    
    * Fix memory leak and validation failures in RAW importer
    
    * Enable tests for fixed RAW files
    
    * Fix memory leak in AMF importer
    
    * Enable tests for fixed AMF files
    
    * Split an increment out of a dereference
    
    * Use unique_ptr for AMF NodeArray
    
    * Add braces
    
    * Split an increment out of a dereference
    
    * Use unique_ptr for AMF MeshArray
    
    * Store AMF child nodes with unique_ptr
    
    * Reduce scope of variables

--- test/unit/ImportExport/RAW/utRAWImportExport.cpp
+++ test/unit/ImportExport/RAW/utRAWImportExport.cpp
@@ -62,3 +62,41 @@ public:
 TEST_F(utRAWImportExport, importSimpleRAWTest) {
     EXPECT_TRUE(importerTest());
 }
+
+
+TEST_F(utRAWImportExport, importInvalid) {
+    Assimp::Importer importer;
+    int flags = 0;
+    flags |= aiProcess_ValidateDataStructure;
+
+    const aiScene *scene = importer.ReadFile(ASSIMP_TEST_MODELS_DIR "/invalid/empty.raw", flags);
+    ASSERT_EQ(scene, nullptr);
+}
+
+
+TEST_F(utRAWImportExport, importColor) {
+    Assimp::Importer importer;
+    int flags = 0;
+    flags |= aiProcess_ValidateDataStructure;
+
+    const aiScene *scene = importer.ReadFile(ASSIMP_TEST_MODELS_DIR "/RAW/WithColor.raw", flags);
+#ifndef ASSIMP_BUILD_NO_RAW_IMPORTER
+    ASSERT_NE(scene, nullptr);
+#else  // ASSIMP_BUILD_NO_RAW_IMPORTER
+    ASSERT_EQ(scene, nullptr);
+#endif  // ASSIMP_BUILD_NO_RAW_IMPORTER
+}
+
+
+TEST_F(utRAWImportExport, importTexture) {
+    Assimp::Importer importer;
+    int flags = 0;
+    flags |= aiProcess_ValidateDataStructure;
+
+    const aiScene *scene = importer.ReadFile(ASSIMP_TEST_MODELS_DIR "/RAW/WithTexture.raw", flags);
+#ifndef ASSIMP_BUILD_NO_RAW_IMPORTER
+    ASSERT_NE(scene, nullptr);
+#else  // ASSIMP_BUILD_NO_RAW_IMPORTER
+    ASSERT_EQ(scene, nullptr);
+#endif  // ASSIMP_BUILD_NO_RAW_IMPORTER
+}

5245d0620fbfac1e6d329985b2af04ea255bd0e0
Ilias <iliasaberkane6@gmail.com>
2026-09-16 20:58:23 +0200

    Fix dropped-property leak and stale refs reuse in parseDataArrayList

--- contrib/openddlparser/code/OpenDDLParser.cpp
+++ contrib/openddlparser/code/OpenDDLParser.cpp
@@ -306,10 +306,12 @@ char *OpenDDLParser::parseHeader(char *in, char *end) {
 
                 if (*in != Grammar::CommaSeparator[0] && *in != Grammar::ClosePropertyToken[0]) {
                     logInvalidTokenError(std::string(in, end), Grammar::ClosePropertyToken, m_logCallback);
+                    delete prop;
+                    delete first;
                     return nullptr;
                 }
 
-                if (nullptr != prop && *in != Grammar::CommaSeparator[0]) {
+                if (nullptr != prop) {
                     if (nullptr == first) {
                         first = prop;
                     }
@@ -1035,6 +1037,7 @@ char *OpenDDLParser::parseDataArrayList(char *in, char *end, Value::ValueType ty
         do {
             size_t numRefs(0), numValues(0);
             currentValue = nullptr;
+            refs = nullptr;
 
             in = parseDataList(in, end, type, &currentValue, numValues, &refs, numRefs);
             if (nullptr != currentValue || 0 != numRefs) {
